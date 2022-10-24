e3482c2d9c84a1045664ee62a568c3b347d590be
onomlight <pass041@nate.com>
2022-10-25 01:47:09 +0900

    comm

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
   };
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <TopNavigationBar cart={cart} />
       <Routes>
         <Route
